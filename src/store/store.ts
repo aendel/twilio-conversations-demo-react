641878299657221e55895df09b6d5c3520516574
Francesco Vattiato <francesco.vattiato@devmy.it>
2023-11-22 14:27:44 +0100

    chore: Add redux-logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers, { initialState } from "./reducers";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const store = createStore(
   reducers,
   initialState,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
 );
